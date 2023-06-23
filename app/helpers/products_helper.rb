67bbd254b3374cdbd1f404ebf2f95993afa721ad
Daniel <sergio.bosco@uap.edu.ar>
2023-06-23 16:28:16 -0300

    diseño

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,19 @@
 module ProductsHelper
+
+    def show_image(product)
+        if product.image.image?
+          image_tag product.image, id: 'procedure-image', style:'height: 400px; width:auto'
+        else
+          image_tag "3", id: 'procedure-image'
+        end
+      end
+
+    def price(product)
+        if product.price != 0
+          return product.price.to_s + "$"
+        else
+          return "Gratis"
+        end
+    end
+
 end
